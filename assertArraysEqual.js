d19e0aba0546187669606e4ae217aae7bb174f11
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-05-27 02:52:29 +0000

    Implementing the function for the first time

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  const answer = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  console.log(answer);
+};
+
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (firstArray, secondArray) => {
+  assertEqual(eqArrays(firstArray, secondArray), true);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
