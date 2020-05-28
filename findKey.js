548bf82ec69caa510be9a953a0c6b4e171045f4b
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-05-28 23:22:09 +0000

    implementation

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  const answer = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  console.log(answer);
+};
+
+const findKey = (obj, callback) => {
+   
+  const keys = Object.keys(obj);
+  
+  for (const key of keys) {
+   
+    if (callback(obj[key])) {
+      return key;
+    }
+  
+  }
+};
+
+// Testing Code
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result1, "noma");
+
