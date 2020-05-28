ebc2df52de1f1e3a8a0e8ad72743cc056c330b70
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-05-28 04:33:47 +0000

    first implementation

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  const answer = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  console.log(answer);
+};
+
+const findKeyByValue = (listToSearch, valueToFind) => {
+  for (const listKeys in listToSearch) {
+
+    if (listToSearch[listKeys] === valueToFind) {
+      return listKeys;
+    } 
+    
+  }
+};
+
+
+// Test case 
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
