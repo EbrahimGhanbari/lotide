400197eb87402864dc68582c06b97ba16dea3739
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-01 22:06:41 +0000

    cleaned up

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,8 @@
-const assertEqual = function(actual, expected) {
-  const answer = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  console.log(answer);
-};
-
-const eqArrays = (firstArray, secondArray) => {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (firstArray, secondArray) => {
   assertEqual(eqArrays(firstArray, secondArray), true);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+module.exports = assertArraysEqual;
