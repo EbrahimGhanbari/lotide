57fc025711449f9d719d1a3c599196f0f34f2908
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-01 23:28:23 +0000

    revised with chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
-// Test Code here 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]);
+const eqArrays = require("../eqArrays");
+
+
+describe("#middle", () => {
+  it('return [2, 3] for [1, 2, 3, 4]', () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    eqArrays(actual, expected);
+  });
+
+  it('return [3] for [1, 2, 3, 4, 5]', () => {
+    const actual = middle([1, 2, 3, 4, 5]);
+    const expected = [3];
+    eqArrays(actual, expected);    
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const expect = require("chai").expect;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("", () => {
+    const actual = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    const expected = ["Lighthouse", "Labs"];
+    expect(actual).to.deep.equal(expected);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it("", () => {
+    const actual = tail([1, 2]);
+    const expected = [2];
+    expect(actual).to.deep.equal(expected);
+  });
+
+});
