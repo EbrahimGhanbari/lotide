b5db11ed814b4461ed46d5ef90d47a2d0c65f7df
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-01 22:38:44 +0000

    revisied

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,5 @@
-
+const middle = require("../middle");
+const assertArraysEqual = require("../assertArraysEqual");
 // Test Code here 
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]);
