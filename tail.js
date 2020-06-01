f124e82456b6d6baae04139be9e60eaf4a35309d
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-01 21:17:34 +0000

    moved testing section and used require fir fun

--- tail.js
+++ tail.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  const answer = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  console.log(answer);
-};
+const assertEqual = require("./assertEqual");
 
 const tail = inputArray => {
   let outputArray = [];
@@ -11,11 +8,4 @@ const tail = inputArray => {
   return outputArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
